a01b3b88327958b3cc9eb65e212c4363a7d8495b
Cris <cristina.romero.calvo@gmail.com>
2019-02-04 18:17:07 +0100

    On click on button green the text changes to green. On long click on button green the text changes to another green.

--- app/src/main/java/com/example/cris_gnss_myphone/DisplayMessageActivity.java
+++ app/src/main/java/com/example/cris_gnss_myphone/DisplayMessageActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -26,11 +27,16 @@ public class DisplayMessageActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.textView);
         textView.setText(message);
 
+
+
 //        **** This is a way of creating a new layout and a new button from here
 //        RelativeLayout myLayout = new RelativeLayout(this);
 //        Button blue_button = new Button(this);
 //
 //        blue_button.setText("Blue");
+
+//        We set an id to the button
+//        blue_button.setId(1);
 //
 //        RelativeLayout.LayoutParams buttonDetails =
 //                new RelativeLayout.LayoutParams(
@@ -44,11 +50,40 @@ public class DisplayMessageActivity extends AppCompatActivity {
 //
 //        setContentView(myLayout);
 
+        Button button_green = findViewById(R.id.button2); // we define out button that we created in the xml
+        button_green.setOnClickListener(
+                // now we add a call back method which is a method that is going to be called when a click event related to this button occurs.
+                new Button.OnClickListener(){
+
+                    // The following is the call back method
+                    public void onClick(View myView){
+                        TextView myText = findViewById(R.id.textView);
+                        myText.setTextColor(Color.GREEN);
+                     }
+                }
+
+        );
+
+        // We are going to use a long click listener which is a button that has to be hold more time to be activated
+        button_green.setOnLongClickListener(
+                new Button.OnLongClickListener(){
+
+                    public boolean onLongClick(View myView){  // now is boolean beacause we expect a true or a false
+                        TextView myText = findViewById(R.id.textView);
+                        myText.setTextColor(Color.parseColor("#009688"));
+                        return true;
+
+                    }
+
+                }
+
+
+        );
+
 
 
 
     }
-
 
 
 }
